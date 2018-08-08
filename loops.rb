3b859c27cf90f4cc3f2d93b37b7014234f998a14
Jon Wenger <jonwenger783@gmail.com>
2018-08-08 11:13:23 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,15 +1,4 @@
-# def dance
-# moves_slay = 0
-# loop do
-#   if moves_slay == 10
-#     break
-#   end
- 
-#   puts "One..."
-#   puts "  and two..."
-#   puts ""
-#   moves_slay = moves_slay + 1
-   
-# end
-# end
-# dance
+5.times do 
+  puts "he loves me"
+  puts "he loves  me not"
+end 
